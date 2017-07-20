255f52135a60a41497aa22b90cd588c3af5f35cf
Junzhe liang <jliang20@wooster.edu>
2017-07-20 10:28:57 +0800

    Put buttons in the main view to stackviews

---
+++ Basketball Trainer/Basketball Trainer/Personalphotobutton.swift
@@ -0,0 +1,22 @@
+//
+//  Personalphotobutton.swift
+//  Basketball Trainer
+//
+//  Created by Tony Liang on 7/15/17.
+//  Copyright © 2017 Tony Liang. All rights reserved.
+//
+
+import UIKit
+
+class Personalphotobutton: UIButton {
+
+    
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
